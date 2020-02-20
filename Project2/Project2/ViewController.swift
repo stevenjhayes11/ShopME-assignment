88f8452267908359055481f45f96cf00abfba20b
stevenjhayes11 <41926543+stevenjhayes11@users.noreply.github.com>
2020-02-20 10:58:49 -0500

    Initial Commit
    Not much done, just assets imported and view controllers added

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Steven Hayes on 2/20/20.
+//  Copyright © 2020 Steven Hayes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
